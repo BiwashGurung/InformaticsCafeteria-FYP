595dbdc99a69a76181235d52a5f633030dba9b4b
BiwashGurung <varungrg10@gmail.com>
2025-02-04 14:54:26 +0545

    FEAT: reorder dashboard URL in cafeteria_admin and clean up settings.py

--- cafeteria_admin/urls.py
+++ cafeteria_admin/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 from .import views
 
 urlpatterns = [
-    path('dashboard/',views.cafeteria_admin_dashboard,name='cafeteria_admin_dashboard'),
     path('login/',views.cafeteria_admin_login,name='cafeteria_admin_login'),
+    path('dashboard/',views.cafeteria_admin_dashboard,name='cafeteria_admin_dashboard'),
 ]
