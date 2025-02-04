2a78916f9f6ab07cd397e264614ecd24f1ce34cb
BiwashGurung <varungrg10@gmail.com>
2025-02-04 15:15:26 +0545

    FEAT: update cafeteria_admin app by commenting out model registration and login logic, and adding model structure

--- cafeteria_admin/models.py
+++ cafeteria_admin/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+#creating models for admin
+# class Admin(models.Model):
+#     username = models.CharField(max_length=150, unique=True)
+#     password = models.CharField(max_length=128)
+#     email = models.EmailField(unique=True)
+#     first_name = models.CharField(max_length=30)
+#     last_name = models.CharField(max_length=30)
+#     date_joined = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.username
+
+#         class Meta:
+#         db_table = 'cafeteria_admin'
--- cafeteria_admin/views.py
+++ cafeteria_admin/views.py
@@ -6,10 +6,4 @@ def cafeteria_admin_dashboard(request):
     return HttpResponse("This is the cafeteria admin dashboard")
 
 def cafeteria_admin_login(request):
-   if request.method == 'POST':
-       username = request.POST.get('username')
-       password = request.POST.get('password')
-       if username == 'informatics' and password == 'informatics@123':
-           return HttpResponse("Login successful")
-       else:
-           return HttpResponse("Login failed")
+    return HttpResponse("This is the cafeteria admin login page")    
