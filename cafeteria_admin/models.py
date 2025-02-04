57ba6ca7f9ac7263c6f12401eb79fbf69576bba7
BiwashGurung <varungrg10@gmail.com>
2025-02-04 16:54:16 +0545

    FEAT: implement cafeteria admin login functionality and create login template

--- cafeteria_admin/models.py
+++ cafeteria_admin/models.py
@@ -1,16 +1,1 @@
 from django.db import models
-
-#creating models for admin
-# class Admin(models.Model):
-#     username = models.CharField(max_length=150, unique=True)
-#     password = models.CharField(max_length=128)
-#     email = models.EmailField(unique=True)
-#     first_name = models.CharField(max_length=30)
-#     last_name = models.CharField(max_length=30)
-#     date_joined = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return self.username
-
-#         class Meta:
-#         db_table = 'cafeteria_admin'
--- cafeteria_admin/views.py
+++ cafeteria_admin/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def cafeteria_admin_dashboard(request):
     return HttpResponse("This is the cafeteria admin dashboard")
 
 def cafeteria_admin_login(request):
-    return HttpResponse("This is the cafeteria admin login page")    
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None and user.is_superuser:
+            login(request, user)
+            return redirect('cafeteria_admin_dashboard')
+    return render(request, 'cafeteria_admin/admin_login.html')
