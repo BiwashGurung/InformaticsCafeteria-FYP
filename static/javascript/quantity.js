49cebdf5b8f250f259a835b7911aeac5e8827816
BiwashGurung <varungrg10@gmail.com>
2025-03-02 12:04:18 +0545

    FEAT: enhance cart functionality by improving quantity adjustment logic and updating cart template to greet user

--- static/javascript/quantity.js
+++ static/javascript/quantity.js
@@ -1,13 +1,16 @@
+function increaseQuantity(button) {
+    let inputField = button.previousElementSibling;
+    let currentValue = parseInt(inputField.value, 10);
+    inputField.value = currentValue + 1;
+}
+
 function decreaseQuantity(button) {
-    let input = button.nextElementSibling;
-    let value = parseInt(input.value);
-    if (value > 1) {
-        input.value = value - 1;
+    let inputField = button.nextElementSibling;
+    let currentValue = parseInt(inputField.value, 10);
+    if (currentValue > 1) {
+        inputField.value = currentValue - 1;
     }
 }
 
-function increaseQuantity(button) {
-    let input = button.previousElementSibling;
-    let value = parseInt(input.value);
-    input.value = value + 1;
-}
+
+
