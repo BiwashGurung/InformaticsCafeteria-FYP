7294b87b0335d2661cdf46be4b486b64d29ec79d
BiwashGurung <varungrg10@gmail.com>
2025-04-24 20:47:52 +0545

    FEAT: Implement password reset functionality with updated templates and improved user experience

--- temp/urls.py
+++ temp/urls.py
@@ -57,8 +57,8 @@ urlpatterns = [
     # Password reset views
     # path('reset_password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     # path('reset_password/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    # path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     path('reset_password/', auth_views.PasswordResetView.as_view(
         template_name='registration/password_reset_form.html',
@@ -69,10 +69,10 @@ urlpatterns = [
     path('reset_password/done/', auth_views.PasswordResetDoneView.as_view(
         template_name='registration/password_reset_done.html'
     ), name='password_reset_done'),
-    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
-    #     template_name='registration/password_reset_confirm.html'
-    # ), name='password_reset_confirm'),
-    # path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
-    #     template_name='registration/password_reset_complete.html'
-    # ), name='password_reset_complete'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='registration/password_reset_confirm.html'
+    ), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='registration/password_reset_complete.html'
+    ), name='password_reset_complete'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
