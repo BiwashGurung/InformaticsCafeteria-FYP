3a7167a725d91edd90f3f274c380b4d4754a39ee
BiwashGurung <varungrg10@gmail.com>
2025-04-29 13:28:50 +0545

    FEAT: Implement cart item update, removal, clearing, and empty cart summary tests

--- cafeteria/tests.py
+++ cafeteria/tests.py
@@ -58,39 +58,39 @@ class CafeteriaViewsTest(TestCase):
         cart_item = CartItem.objects.get(cart=cart, food_item=self.food_item)
         self.assertEqual(cart_item.quantity, 2)
 
-    # def test_update_cart_item(self):
-    #     self.client.login(username='testuser', password='testpass')
-    #     cart = Cart.objects.create(user=self.user)
-    #     cart_item = CartItem.objects.create(cart=cart, food_item=self.food_item, quantity=1)
+    def test_update_cart_item(self):
+        self.client.login(username='testuser', password='testpass')
+        cart = Cart.objects.create(user=self.user)
+        cart_item = CartItem.objects.create(cart=cart, food_item=self.food_item, quantity=1)
 
-    #     response = self.client.post(reverse('update_cart', args=[cart_item.id]), {
-    #         'quantity': '5'
-    #     })
-    #     self.assertEqual(response.status_code, 302)
-    #     cart_item.refresh_from_db()
-    #     self.assertEqual(cart_item.quantity, 5)
+        response = self.client.post(reverse('update_cart', args=[cart_item.id]), {
+            'quantity': '5'
+        })
+        self.assertEqual(response.status_code, 302)
+        cart_item.refresh_from_db()
+        self.assertEqual(cart_item.quantity, 5)
 
-    # def test_remove_from_cart(self):
-    #     self.client.login(username='testuser', password='testpass')
-    #     cart = Cart.objects.create(user=self.user)
-    #     cart_item = CartItem.objects.create(cart=cart, food_item=self.food_item, quantity=1)
+    def test_remove_from_cart(self):
+        self.client.login(username='testuser', password='testpass')
+        cart = Cart.objects.create(user=self.user)
+        cart_item = CartItem.objects.create(cart=cart, food_item=self.food_item, quantity=1)
 
-    #     response = self.client.get(reverse('remove_from_cart', args=[cart_item.id]))
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertFalse(CartItem.objects.filter(id=cart_item.id).exists())
+        response = self.client.get(reverse('remove_from_cart', args=[cart_item.id]))
+        self.assertEqual(response.status_code, 302)
+        self.assertFalse(CartItem.objects.filter(id=cart_item.id).exists())
 
-    # def test_clear_cart(self):
-    #     self.client.login(username='testuser', password='testpass')
-    #     cart = Cart.objects.create(user=self.user)
-    #     CartItem.objects.create(cart=cart, food_item=self.food_item, quantity=2)
+    def test_clear_cart(self):
+        self.client.login(username='testuser', password='testpass')
+        cart = Cart.objects.create(user=self.user)
+        CartItem.objects.create(cart=cart, food_item=self.food_item, quantity=2)
 
-    #     response = self.client.get(reverse('clear_cart'))
-    #     self.assertEqual(response.status_code, 302)
-    #     self.assertEqual(cart.cart_items.count(), 0)
+        response = self.client.get(reverse('clear_cart'))
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(cart.cart_items.count(), 0)
 
-    # def test_cart_summary_empty(self):
-    #     self.client.login(username='testuser', password='testpass')
-    #     response = self.client.get(reverse('cartsummary'))
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertContains(response, "Your cart is empty.")
+    def test_cart_summary_empty(self):
+        self.client.login(username='testuser', password='testpass')
+        response = self.client.get(reverse('cartsummary'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Your cart is empty.")
 
